02060072b1e77cdc0e403021235448a24536a234
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2020-12-13 14:20:55 +0530

    Deleted unnecessary dummy data

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,4 @@
 
-
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
-// fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
-//     response.json().then((data)=>{
-//         if(data.error){
-//             console.log(data.error)
-//         }else{
-//             console.log(data.country)
-//             console.log(data.temperature)
-//         }
-//     })
-// })
-
 const weatherForm=document.querySelector('form')
 const search= document.querySelector('input')
 const messageOne= document.querySelector('#message-1')
